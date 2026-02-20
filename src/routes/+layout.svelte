5a60920dd8655627f6449f94c3468b8df1ade95e
muzikp <muzikp@gmail.com>
2026-02-20 18:14:45 +0100

    fix: move favicon to layout using base from \/paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
+	import { base } from '$app/paths';
 	import '../app.css';
 </script>
+
+<svelte:head>
+	<link rel="icon" href="{base}/favicon.png" />
+</svelte:head>
 
 <div class="app">
 	<slot />
